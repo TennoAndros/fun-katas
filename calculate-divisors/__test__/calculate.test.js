1c0ed46b6bf125c2671e88f6821c10a5299d8f6d
Andreas <andreasjr77@outlook.com>
2023-04-17 15:37:17 +0300

    Better

--- calculate-divisors/__test__/calculate.test.js
+++ calculate-divisors/__test__/calculate.test.js
@@ -1,12 +1,15 @@
 const calculateDivisors = require("../calculate-divisors.js");
 
-describe.only("calculateDivisors", () => {
+describe("calculateDivisors", () => {
   test("should return a number", () => {
     expect(typeof calculateDivisors()).toBe("number");
   });
   test("calculate divisors of 3 and 5", () => {
-    expect(calculateDivisors(4)).toBe(3);
+    expect(calculateDivisors(1)).toBe(0);
+    expect(calculateDivisors(5)).toBe(3);
     expect(calculateDivisors(6)).toBe(8);
-    expect(calculateDivisors(16)).toBe(3 + 5 + 6 + 9 + 10 + 12 + 15);
+    expect(calculateDivisors(10)).toBe(3 + 5 + 6 + 9);
+    expect(calculateDivisors(12)).toBe(3 + 5 + 6 + 9 + 10);
+    expect(calculateDivisors(15)).toBe(3 + 5 + 6 + 9 + 10 + 12);
   });
 });
