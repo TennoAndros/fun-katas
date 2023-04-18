b587eb0d694b677d104a428a9e3db4ecc74a8d0a
Andreas <andreasjr77@outlook.com>
2023-04-18 10:19:56 +0300

    solution to change cal
    Co-authored-by: Nondas B <donblizy@users.noreply.github.com>

--- change-calculator/__test__/change.test.js
+++ change-calculator/__test__/change.test.js
@@ -1,6 +1,6 @@
 const changeCalculator = require("../change-calculator");
 
-describe("calculate change", () => {
+describe.only("calculate change", () => {
   test("should return an object", () => {
     expect(typeof changeCalculator()).toEqual("object");
   });
@@ -9,5 +9,7 @@ describe("calculate change", () => {
     expect(changeCalculator(2)).toEqual({ 2: 1 });
     expect(changeCalculator(7)).toEqual({ 5: 1, 2: 1 });
     expect(changeCalculator(13)).toEqual({ 10: 1, 2: 1, 1: 1 });
+    expect(changeCalculator(44)).toEqual({ 20: 2, 2: 2});
+
   });
 });
--- change-calculator/change-calculator.js
+++ change-calculator/change-calculator.js
@@ -1,22 +1,37 @@
 // Please do not change the name of this function
 function changeCalculator(num) {
-  let change = {};
-  for (let i = 0; i < num; i++) {
-    if (num === 1) {
-      change["1"] = 1;
+  let change = {
+    50: 0,
+    20: 0,
+    10: 0,
+    5: 0,
+    2: 0,
+    1: 0,
+  };
+
+  while (num > 0) {
+    if (num >= 50) {
+      change["50"] += 1;
+      num -= 50;
+    } else if (num < 50 && num > 20) {
+      change["20"] += 1;
+      num -= 20;
+    } else if (num < 20 && num > 9) {
+      change["10"] += 1;
+      num -= 10;
+    } else if (num < 10 && num > 4) {
+      change["5"] += 1;
+      num -= 5;
+    } else if (num < 5 && num >= 2) {
+      change["2"] += 1;
+      num -= 2;
+    } else {
+      change["1"] += 1;
+      num -= 1;
     }
-    if (num === 2) {
-      change["2"] = 1;
-    }
-    if (num === 7) {
-      change["5"] = 1;
-      change["2"] = 1;
-    }
-    if (num === 13) {
-      change["10"] = 1;
-      change["2"] = 1;
-      change["1"] = 1;
-    }
+  }
+  for (let key in change) {
+    if (change[key] === 0) delete change[key];
   }
   console.log(change);
   return change;
